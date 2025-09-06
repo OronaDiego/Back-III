8ead0b6786f791783f22f95e3e8a7880472d98ed
Diego <diegosebastianorona@gmail.com>
2025-09-06 10:56:30 -0300

    install dependences for proyect

--- proyecto-final/src/server.ts
+++ proyecto-final/src/server.ts
@@ -1,1 +1,11 @@
 import express from 'express';
+
+
+const app= express();
+const PORT= 8000;
+
+app.get('/', (_req,res)=>{
+    res.send('Hello word')
+})
+
+app.listen(PORT, ()=> console.log(`Server is running on http://localhost:${PORT}`));
