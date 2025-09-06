1c1fdb42cfae2930e530dbb4744f062481a3186f
Diego <diegosebastianorona@gmail.com>
2025-09-06 11:58:21 -0300

    Dejar de trackear carpeta dist

--- proyecto-final/dist/server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const app = express();
-const PORT = 8000;
-app.get('/', (_req, res) => {
-    res.send('Hello word');
-});
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
-//# sourceMappingURL=server.js.map
